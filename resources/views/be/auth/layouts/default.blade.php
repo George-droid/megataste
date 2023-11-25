5c8f13c407a90fc6615e4b1e42e3c5ef601ee10a
Wisdom George <wisgeorge.wg@gmail.com>
2023-11-25 20:01:12 +0100

    Implemented the logout

--- resources/views/be/auth/layouts/default.blade.php
+++ resources/views/be/auth/layouts/default.blade.php
@@ -108,7 +108,7 @@
                         </button>
     
                         <!-- Topbar Search -->
-                        <form
+                        {{-- <form
                             class="d-none d-sm-inline-block form-inline mr-auto ml-md-3 my-2 my-md-0 mw-100 navbar-search">
                             <div class="input-group">
                                 <input type="text" class="form-control bg-light border-0 small" placeholder="Search for..."
@@ -119,7 +119,7 @@
                                     </button>
                                 </div>
                             </div>
-                        </form>
+                        </form> --}}
     
                         <!-- Topbar Navbar -->
                         <ul class="navbar-nav ml-auto">
@@ -154,7 +154,7 @@
                             <li class="nav-item dropdown no-arrow">
                                 <a class="nav-link dropdown-toggle" href="#" id="userDropdown" role="button"
                                     data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                                    <span class="mr-2 d-none d-lg-inline text-gray-600 small">Douglas McGee</span>
+                                    <span class="mr-2 d-none d-lg-inline text-gray-600 small">Admin</span>
                                     <img class="img-profile rounded-circle"
                                         src="img/undraw_profile.svg">
                                 </a>
@@ -174,7 +174,7 @@
                                         Activity Log
                                     </a>
                                     <div class="dropdown-divider"></div> --}}
-                                    <a class="dropdown-item" href="#" data-toggle="modal" data-target="#logoutModal">
+                                    <a class="dropdown-item" href="{{ route('logout') }}" data-toggle="modal" data-target="#logoutModal">
                                         <i class="fas fa-sign-out-alt fa-sm fa-fw mr-2 text-gray-400"></i>
                                         Logout
                                     </a>
@@ -226,7 +226,7 @@
                     <div class="modal-body">Select "Logout" below if you are ready to end your current session.</div>
                     <div class="modal-footer">
                         <button class="btn btn-secondary" type="button" data-dismiss="modal">Cancel</button>
-                        <a class="btn btn-primary" href="login.html">Logout</a>
+                        <a class="btn btn-primary" href="{{ route('login') }}">Logout</a>
                     </div>
                 </div>
             </div>
