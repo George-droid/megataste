ffba3feb2c8fd4e4fd6e3dcad3b4cc9cd758aece
Wisdom George <wisgeorge.wg@gmail.com>
2023-12-01 08:42:00 +0100

    Created the order_dishes table

---
+++ app/Http/Controllers/OrderDishController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\OrderDish;
+use App\Http\Requests\StoreOrderDishRequest;
+use App\Http\Requests\UpdateOrderDishRequest;
+
+class OrderDishController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreOrderDishRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(OrderDish $orderDish)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(OrderDish $orderDish)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateOrderDishRequest $request, OrderDish $orderDish)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(OrderDish $orderDish)
+    {
+        //
+    }
+}
